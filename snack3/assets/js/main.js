5eecc47327fca21314c09fab4ed3f84a3dbf5f02
RobertoNesta <robertonesta1993@gmail.com>
2023-03-23 12:57:15 +0100

    initial commit adding the first array with animals

---
+++ snack3/assets/js/main.js
@@ -0,0 +1,44 @@
+/*snack 3
+Crea un array di oggetti che rappresentano degli animali.
+Ogni animale ha un nome, una famiglia e una classe.
+Es:
+[
+  { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+  { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+  { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+]
+Crea un nuovo array con la lista dei mammiferi.*/
+
+//array animali
+
+const animals = [
+    { nome: 'leone',
+     famiglia: 'felidi',
+     classe: 'mammiferi'
+    },
+
+    { nome: 'cane',
+     famiglia: 'canidi',
+     classe: 'mammiferi'
+    },
+
+    { nome: 'gallina',
+     famiglia: 'fasianidi',
+     classe: 'uccelli'
+    },
+  ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
