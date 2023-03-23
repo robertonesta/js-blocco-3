d64ecf48a2878c54e4019aa960815a40ceba6d1f
RobertoNesta <robertonesta1993@gmail.com>
2023-03-23 12:26:23 +0100

    adding the array and creating new uppercase letter

--- snack2/assets/js/main.js
+++ snack2/assets/js/main.js
@@ -1,3 +1,16 @@
 /*A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l’iniziale maiuscola.
 
 Es: [‘pippo’, ‘PLUTO’, ‘Paperino’] => [‘Pippo’, ‘Pluto’, ‘Paperino’]*/
+
+const characters = [
+    "pippo",
+    "PLUTO",
+    "Paperino",
+    "miNNie",
+    "dAiSy dUcK"
+]
+console.log(characters)
+const first_uppercase_letter = characters.map((character) =>{
+    
+})
+console.log(first_uppercase_letter)
